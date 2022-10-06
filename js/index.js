25168c7428a8b84c712836ee66b00f11dfb5591e
kumarprashant1998 <76582300+kumarprashant1998@users.noreply.github.com>
2022-10-06 16:25:46 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -22,13 +22,14 @@ $(document).ready(() => {
     };
     let songIndex = 1
     let selectedSong = audioOptions["song_" + songIndex];
+
     function startplayer() {
         player = $('#music-player')[0];
         player.controls = false;
     }
 
     function playPauseAudio() {
-        songIndex = $(this).closest('.song')[0].id;
+        songIndex = parseInt($(this).closest('.song')[0].id);
         selectedSong = audioOptions["song_" + songIndex];
         if ($(this).hasClass('playing')) {
             $('.play-pause-icon').removeClass('playing');
@@ -38,6 +39,7 @@ $(document).ready(() => {
                     $(".play-control .play-pause-icon").removeClass("playing")
                     $(".play-control .play-pause-icon i").removeClass('fa-pause').addClass('fa-play');
                     $(this).find('i').removeClass('fa-pause').addClass('fa-play');
+                    songIndex = parseInt($(this).closest('.song')[0].id);
                 }
             })
             $(this).find('i').removeClass('fa-pause').addClass('fa-play');
@@ -57,19 +59,18 @@ $(document).ready(() => {
                     $(".play-control .play-pause-icon").addClass("playing")
                     $(".play-control .play-pause-icon i").removeClass('fa-play').addClass('fa-pause');
                     $(this).find('i').removeClass('fa-play').addClass('fa-pause');
+                    songIndex = parseInt($(this).closest('.song')[0].id);
                 }
             })
-
             player.play();
         }
     }
 
     function previousSong() {
-
+        $(`#${songIndex-1} .play-pause-icon`).click();
     }
     function nextSong() {
-        selectedSong = audioOptions[`song_${++songIndex}`]
-        playPauseAudio();
+        $(`#${songIndex+1} .play-pause-icon`).click();
     }
 
 
@@ -83,7 +84,15 @@ $(document).ready(() => {
         $('#album-title').text($(this).siblings(".album-name").text());
     });
 });
-function seek(value) {
-    let seekto = player.duration * (value / 100);
+
+$("#seekAudio").on("change",seek)
+
+function seek(){
+    let seekto = player.duration * ($(this)[0].value / 100);
     player.currentTime = seekto;
 }
+
+setInterval(()=>{
+    $("#seekAudio")[0].value = player.currentTime/player.duration*100;
+},1000)
+
